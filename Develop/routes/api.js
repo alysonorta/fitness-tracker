4836ef03ff7d9742f47d0e617943cb9b33c595a7
Alyson Orta <alyson.orta@gmail.com>
2021-09-28 15:41:58 -0400

    updated root route to match api file in public and removed view.js from server file

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post('/api/workout', ({ body }, res) => {
+router.post('/api/workouts', ({ body }, res) => {
     Workout.create(body)
-    .then()
-})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.get('/api/workouts', (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+module.exports = router;
